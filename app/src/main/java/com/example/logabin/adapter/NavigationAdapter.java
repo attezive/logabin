13ffe6ae53b3eaddc64ca8612dfd3b49f3f4da0f
Attezive <attezive@gmail.com>
2024-05-18 16:45:42 +0300

    Thr fragments are now saved Version v0.2.1

--- app/src/main/java/com/example/logabin/adapter/NavigationAdapter.java
+++ app/src/main/java/com/example/logabin/adapter/NavigationAdapter.java
@@ -1,5 +1,8 @@
 package com.example.logabin.adapter;
 
+import android.util.Log;
+import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -30,4 +33,8 @@ public class NavigationAdapter extends FragmentPagerAdapter {
         fragmentList.add(fragment);
     }
 
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        // We mustn`t destroy the fragments
+    }
 }
